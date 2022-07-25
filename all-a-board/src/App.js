1feff5d43ae05f3b559a83e2e70ea4528087ad9d
DanMossDev <danielsmoss96@gmail.com>
2022-07-25 15:26:10 +0100

    Implement routing endpoints for various categories

--- all-a-board/src/App.js
+++ all-a-board/src/App.js
@@ -1,18 +1,31 @@
-import './App.css';
-import { useState } from 'react';
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
+import './App.css'
+import { useState, useEffect } from 'react'
+import { getCategories } from './axios'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1) 
 
   return (
+    <BrowserRouter>
     <div className="App">
       <Header />
-      <Main currentPage={currentPage}/>
+      <Routes>
+        <Route path={'/'} element={<Main currentPage={currentPage}/>}/>
+        <Route path={'/strategy'} element={<Main currentPage={currentPage} category='strategy'/>}/>
+        <Route path={'/hidden-roles'} element={<Main currentPage={currentPage} category='hidden-roles'/>}/>
+        <Route path={'/dexterity'} element={<Main currentPage={currentPage} category='dexterity'/>}/>
+        <Route path={'/push-your-luck'} element={<Main currentPage={currentPage} category='push-your-luck'/>}/>
+        <Route path={'/roll-and-write'} element={<Main currentPage={currentPage} category='roll-and-write'/>}/>
+        <Route path={'/deck-building'} element={<Main currentPage={currentPage} category='deck-building'/>}/>
+        <Route path={'/engine-building'} element={<Main currentPage={currentPage} category='engine-building'/>}/>
+      </Routes>
       <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
--- all-a-board/src/components/Main/Main.jsx
+++ all-a-board/src/components/Main/Main.jsx
@@ -1,26 +1,33 @@
 import {useState, useEffect} from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { getCategories, getReviews } from '../../axios'
 import ReviewCard from './ReviewCard'
 
-export default function Main({currentPage}) {
+export default function Main({currentPage, category}) {
     const [reviews, setReviews] = useState([])
     const [selectedReview, setSelectedReview] = useState()
-    const [categories, setCategories] = useState([])
-    const [category, setCategory] = useState()
     const [sortBy, setSortBy] = useState()
     const [order, setOrder] = useState()
+    const [categories, setCategories] = useState([])
+    const navigate = useNavigate()
+    const location = useLocation()
 
     useEffect(() => {
         getCategories().then(({data}) => setCategories(data))
     }, [])
-
+  
     useEffect(() => {
         getReviews(currentPage, category, sortBy, order).then(({data}) => setReviews(data))
     }, [currentPage, category, sortBy, order])
 
+    function handleCategory(e) {
+        const category = e.target.value
+        navigate(`../${e.target.value}`, {replace: true})
+    }
+
     return <main>
         <label htmlFor="category-select">Category: </label>
-        <select onChange={e => setCategory(e.target.value)} name="category" id="category-select">
+        <select onChange={handleCategory} name="category" id="category-select">
         <option value="">Show All</option>
         {categories.map((category) => {
             return <option key={category.slug} value={category.slug}>{category.slug}</option>
@@ -28,7 +35,7 @@ export default function Main({currentPage}) {
         </select>
         <section>
             {reviews.map(review => {
-                return <ReviewCard title={review.title} imageURL={review.review_img_url} category={review.category} author={review.owner}/>
+                return <ReviewCard key={review.review_id} title={review.title} imageURL={review.review_img_url} category={review.category} author={review.owner}/>
             })}
         </section>
         
