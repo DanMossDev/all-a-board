4de8e30029354f826b42cabd9e4e3eb6f2d94ba5
DanMossDev <danielsmoss96@gmail.com>
2022-07-25 16:17:50 +0100

    Refactor to implement feedback

--- all-a-board/src/App.js
+++ all-a-board/src/App.js
@@ -1,7 +1,7 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 import { getCategories } from './axios'
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useParams } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
@@ -12,16 +12,10 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <Header />
+      <Header/>
       <Routes>
         <Route path={'/'} element={<Main currentPage={currentPage}/>}/>
-        <Route path={'/strategy'} element={<Main currentPage={currentPage} category='strategy'/>}/>
-        <Route path={'/hidden-roles'} element={<Main currentPage={currentPage} category='hidden-roles'/>}/>
-        <Route path={'/dexterity'} element={<Main currentPage={currentPage} category='dexterity'/>}/>
-        <Route path={'/push-your-luck'} element={<Main currentPage={currentPage} category='push-your-luck'/>}/>
-        <Route path={'/roll-and-write'} element={<Main currentPage={currentPage} category='roll-and-write'/>}/>
-        <Route path={'/deck-building'} element={<Main currentPage={currentPage} category='deck-building'/>}/>
-        <Route path={'/engine-building'} element={<Main currentPage={currentPage} category='engine-building'/>}/>
+        <Route path={'/:category'} element={<Main currentPage={currentPage}/>}/>
       </Routes>
       <Footer />
     </div>
--- all-a-board/src/components/Main/Main.jsx
+++ all-a-board/src/components/Main/Main.jsx
@@ -1,16 +1,19 @@
+import './Main.css'
 import {useState, useEffect} from 'react'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate, useParams, Link } from 'react-router-dom'
 import { getCategories, getReviews } from '../../axios'
 import ReviewCard from './ReviewCard'
+import ReviewPage from './ReviewPage'
 
-export default function Main({currentPage, category}) {
+export default function Main({currentPage}) {
     const [reviews, setReviews] = useState([])
     const [selectedReview, setSelectedReview] = useState()
     const [sortBy, setSortBy] = useState()
     const [order, setOrder] = useState()
     const [categories, setCategories] = useState([])
     const navigate = useNavigate()
-    const location = useLocation()
+
+    const {category} = useParams()
 
     useEffect(() => {
         getCategories().then(({data}) => setCategories(data))
@@ -20,24 +23,25 @@ export default function Main({currentPage, category}) {
         getReviews(currentPage, category, sortBy, order).then(({data}) => setReviews(data))
     }, [currentPage, category, sortBy, order])
 
-    function handleCategory(e) {
-        const category = e.target.value
-        navigate(`../${e.target.value}`, {replace: true})
-    }
-
     return <main>
-        <label htmlFor="category-select">Category: </label>
-        <select onChange={handleCategory} name="category" id="category-select">
-        <option value="">Show All</option>
-        {categories.map((category) => {
-            return <option key={category.slug} value={category.slug}>{category.slug}</option>
-        })}
-        </select>
+        {!selectedReview ? 
+        <>
+        <nav>
+            <ul>
+                {categories.map((category) => {
+                    return <li key={category.slug} value={category.slug}><Link to={`${category.slug}`} replace>{category.slug}</Link></li>
+                })}
+            </ul>
+        </nav>
+        
         <section>
             {reviews.map(review => {
-                return <ReviewCard key={review.review_id} title={review.title} imageURL={review.review_img_url} category={review.category} author={review.owner}/>
+                return <ReviewCard review={review} setSelectedReview={setSelectedReview} key={review.review_id} title={review.title} imageURL={review.review_img_url} category={review.category} author={review.owner}/>
             })}
         </section>
+        </>
+        : <ReviewPage selectedReview={selectedReview}/>
+        }
         
     </main>
 }
