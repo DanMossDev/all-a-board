3f7a12d600dead715d6cc364929ed485f9b4bcde
DanMossDev <danielsmoss96@gmail.com>
2022-07-26 11:03:44 +0100

    Implement ability to vote on reviews, optimistically rendered

--- all-a-board/src/components/Main/VoteBox.jsx
+++ all-a-board/src/components/Main/VoteBox.jsx
@@ -1,3 +1,4 @@
+import './VoteBox.css'
 import { useState } from "react";
 import { reviewVote } from '../../axios'
 export default function VoteBox({currentVotes, review_id}) {
@@ -9,8 +10,8 @@ export default function VoteBox({currentVotes, review_id}) {
     }
 
     return <section className="vote">
-        <button onClick={() => handleVote(1)}>Up</button>
+        <button className="vote-arrow" onClick={() => handleVote(1)}>Up</button>
         <h3>{votes}</h3>
-        <button onClick={() => handleVote(-1)}>Dn</button>
+        <button className="vote-arrow" onClick={() => handleVote(-1)}>Dn</button>
     </section>
 }
